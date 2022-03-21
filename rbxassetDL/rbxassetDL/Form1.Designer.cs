b4d5dfb5062c7e4fe6927cef58e5fbd68953767c
AustinnK <35577494+AustinnK@users.noreply.github.com>
2022-03-20 22:50:37 -0700

    Add files via upload

---
+++ rbxassetDL/rbxassetDL/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace rbxassetDL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.getassetsbutton = new System.Windows.Forms.Button();
+            this.URLStextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.filepathbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // getassetsbutton
+            // 
+            this.getassetsbutton.Location = new System.Drawing.Point(628, 420);
+            this.getassetsbutton.Name = "getassetsbutton";
+            this.getassetsbutton.Size = new System.Drawing.Size(147, 27);
+            this.getassetsbutton.TabIndex = 0;
+            this.getassetsbutton.Text = "Download";
+            this.getassetsbutton.UseVisualStyleBackColor = true;
+            this.getassetsbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // URLStextBox
+            // 
+            this.URLStextBox.AccessibleDescription = "";
+            this.URLStextBox.Location = new System.Drawing.Point(12, 29);
+            this.URLStextBox.Multiline = true;
+            this.URLStextBox.Name = "URLStextBox";
+            this.URLStextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.URLStextBox.Size = new System.Drawing.Size(764, 162);
+            this.URLStextBox.TabIndex = 1;
+            this.URLStextBox.WordWrap = false;
+            this.URLStextBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 204);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(763, 183);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Paste audio assetIDs in the field below. Seperate multiple assetIDs with a new li" +
+    "ne.";
+            // 
+            // filepathbox
+            // 
+            this.filepathbox.Location = new System.Drawing.Point(65, 394);
+            this.filepathbox.Name = "filepathbox";
+            this.filepathbox.ReadOnly = true;
+            this.filepathbox.Size = new System.Drawing.Size(710, 20);
+            this.filepathbox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "file path:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.filepathbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.URLStextBox);
+            this.Controls.Add(this.getassetsbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Roblox Asset Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getassetsbutton;
+        private System.Windows.Forms.TextBox URLStextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox filepathbox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ rbxassetDL/rbxassetDL/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace rbxassetDL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            filepathbox.Text = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\Downloaded";
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\Downloaded");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = URLStextBox.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                using (Process p = new Process())
+                {
+                    p.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(Application.ExecutablePath); ;
+                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.Arguments = "/c node getasset.js " + lines[i];
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.Start();
+                    StreamReader reader = p.StandardOutput;
+                    string output;
+                    while ((output = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(output);
+                        richTextBox1.Text = richTextBox1.Text + output + "\n";
+                        using (var client = new WebClient())
+                        {
+                            client.DownloadFile(output, System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\Downloaded\" + lines[i]+".mp3");
+                        }
+                    }
+                }
+            }
+            richTextBox1.Text = richTextBox1.Text + "\n";
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
